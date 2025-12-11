ea8eca8a8d2505c3e3a22f2104a2e7cf215ed1ea
ChaCha <allego02272@gmail.com>
2025-12-11 12:43:45 +0800

    fix: FGController api method

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -43,19 +43,20 @@ Route::prefix('vg')->group(function () {
 });
 
 Route::prefix('fg')->group(function () {
-    Route::post('/signUp', [FGController::class, 'signUp']);
-    Route::post('/clearPlayerSession', [FGController::class, 'clearPlayerSession']);
-    Route::post('/points', [FGController::class, 'points']);
-    Route::post('/balance', [FGController::class, 'balance']);
-    Route::post('/checkPlayerExists', [FGController::class, 'checkPlayerExists']);
-    Route::post('/pointsLog', [FGController::class, 'pointsLog']);
-    Route::post('/checkUnsettled', [FGController::class, 'checkUnsettled']);
-    Route::post('/launchGame', [FGController::class, 'launchGame']);
-    Route::post('/launchFreeGame', [FGController::class, 'launchFreeGame']);
-    Route::post('/gameList', [FGController::class, 'gameList']);
-    Route::post('/appDownloadQRCode', [FGController::class, 'appDownloadQRCode']);
-    Route::post('/appLoginQRCode', [FGController::class, 'appLoginQRCode']);
-    Route::post('/launchLobby', [FGController::class, 'launchLobby']);
+    Route::post('/launchGame', [FGController::class, 'launchGame']); // 3.1 啟動遊戲
+    Route::post('/launchFreeGame', [FGController::class, 'launchFreeGame']); // 3.2 啟動試玩遊戲
+    Route::post('/gameList', [FGController::class, 'gameList']); // 3.3 獲取遊戲列表
+    Route::post('/appDownloadQRCode', [FGController::class, 'appDownloadQRCode']); // 3.4 APP 大廳下載二維碼
+    Route::post('/appLoginQRCode', [FGController::class, 'appLoginQRCode']); // 3.5 APP 登入二維碼
+    Route::post('/launchLobby', [FGController::class, 'launchLobby']); // 3.7 啟動大廳
+
+    Route::post('/signUp', [FGController::class, 'signUp']); // 5.1 註冊用戶
+    Route::post('/clearPlayerSession', [FGController::class, 'clearPlayerSession']); // 5.2 刪除玩家會話
+    Route::post('/points', [FGController::class, 'points']); // 5.3 存取玩家籌碼
+    Route::post('/balance', [FGController::class, 'balance']); // 5.4 查詢玩家籌碼
+    Route::post('/checkPlayerExists', [FGController::class, 'checkPlayerExists']); // 5.5 檢查用戶是否存在
+    Route::post('/pointsLog', [FGController::class, 'pointsLog']); // 5.6 查詢玩家籌碼記錄
+    Route::post('/checkUnsettled', [FGController::class, 'checkUnsettled']); // 5.7 查詢未結算籌碼
 });
 
 Route::prefix('gash')->group(function () {
